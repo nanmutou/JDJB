558734b352db881c346263227bec6b10b3560bdb
KingRan <754879133@qq.com>
2021-12-28 11:56:55 +0800

    更新

--- jd_health.js
+++ jd_health.js
@@ -23,7 +23,7 @@ const $ = new Env("东东健康社区");
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
 const notify = $.isNode() ? require('./sendNotify') : "";
 let cookiesArr = [], cookie = "", allMessage = "", message;
-let reward = process.env.JD_HEALTH_REWARD_NAME ? process.env.JD_HEALTH_REWARD_NAME : '';
+let reward = process.env.JD_HEALTH_REWARD_NAME ? process.env.JD_HEALTH_REWARD_NAME : ''
 const randomCount = $.isNode() ? 20 : 5;
 $.newShareCodes = [];
 let UserShareCodes = "";
@@ -34,27 +34,23 @@ function oc(fn, defaultVal) { //optioanl chaining
 		return undefined
 	}
 }
-function nc(val1, val2) { //nullish coalescing
-	return val1 ? val1 : val2
+function nc(val1, val2) {//nullish coalescing
+  return val1 != undefined ? val1 : val2
 }
 if ($.isNode()) {
-	Object.keys(jdCookieNode).forEach((item) => {
-		cookiesArr.push(jdCookieNode[item]);
-	});
-	if (process.env.JD_DEBUG && process.env.JD_DEBUG === "false")
-		console.log = () => {};
+  Object.keys(jdCookieNode).forEach((item) => {
+    cookiesArr.push(jdCookieNode[item]);
+  });
+  if (process.env.JD_DEBUG && process.env.JD_DEBUG === "false") console.log = () => {};
 } else {
 	cookiesArr = [$.getdata("CookieJD"), $.getdata("CookieJD2"), ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
 const JD_API_HOST = "https://api.m.jd.com/";
-
-!(async() => {
-	if (!cookiesArr[0]) {
-		$.msg($.name, "【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取", "https://bean.m.jd.com/", {
-			"open-url": "https://bean.m.jd.com/"
-		});
-		return;
-	}
+!(async () => {
+  if (!cookiesArr[0]) {
+    $.msg($.name, "【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取", "https://bean.m.jd.com/", {"open-url": "https://bean.m.jd.com/"});
+    return;
+  }
 	console.log(`开始执行任务....\n`);
 	for (let i = 0; i < cookiesArr.length; i++) {
 		if (cookiesArr[i]) {
@@ -80,29 +76,27 @@ const JD_API_HOST = "https://api.m.jd.com/";
 
 async function main() {
 	try {
-		$.score = 0;
-		$.earn = false;
-		await getTaskDetail(-1);
-		await getTaskDetail(16);
-		UserShareCodes = "";
-		await getTaskDetail(6);
-		for (let i = 0; i < 5; ++i) {
-			$.canDo = false;
-			await getTaskDetail();
-			if (!$.canDo)
-				break;
-			await $.wait(1000);
-		}
-		await collectScore();
-		await getTaskDetail(22);
-		await getTaskDetail(-1);
+    $.score = 0
+    $.earn = false
+    await getTaskDetail(-1)
+    await getTaskDetail(16)
+    await getTaskDetail(6)
+    for(let i = 0 ; i < 5; ++i){
+      $.canDo = false
+      await getTaskDetail()
+      if(!$.canDo) break
+      await $.wait(1000)
+    }
+    await collectScore()
+    await getTaskDetail(22);
+    await getTaskDetail(-1)
 
-		if (reward) {
-			await getCommodities();
-		}
+    if (reward) {
+      await getCommodities()
+    }
 
-	} catch (e) {
-		$.logErr(e)
+  } catch (e) {
+    $.logErr(e)
 	}
 }
 
@@ -114,228 +108,180 @@ function showMsg() {
 	})
 }
 
-function GetShareCode(taskId = 6) {
-	return new Promise(resolve => {
-		$.get(taskUrl('jdhealth_getTaskDetail', {
-				"buildingId": "",
-				taskId: taskId === -1 ? '' : taskId,
-				"channelId": 1
-			}),
-			async(err, resp, data) => {
-			try {
-				if (safeGet(data)) {
-					data = $.toObj(data);
-
-					if (oc(() => data.data.result.taskVos)) {
-						console.log(`【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${oc(() => data.data.result.taskVos[0].assistTaskDetailVo.taskToken)}\n`);
-						var strSharedcode = `${oc(() => data.data.result.taskVos[0].assistTaskDetailVo.taskToken)}`;
-						$.newShareCodes.push(strSharedcode);
-					}
-
-				}
-			} catch (e) {
-				console.log(e)
-			}
-			finally {
-				resolve()
-			}
-		})
-	})
-}
-
 function getTaskDetail(taskId = '') {
-	return new Promise(resolve => {
-		$.get(taskUrl('jdhealth_getTaskDetail', {
-				"buildingId": "",
-				taskId: taskId === -1 ? '' : taskId,
-				"channelId": 1
-			}),
-			async(err, resp, data) => {
-			try {
-				if (safeGet(data)) {
-					data = $.toObj(data)
-						if (taskId === -1) {
-							let tmp = parseInt(parseFloat(nc(oc(() => data.data.result.userScore), '0')))
-								if (!$.earn) {
-									$.score = tmp
-										$.earn = 1
-								} else {
-									$.earn = tmp - $.score
-										$.score = tmp
-								}
-						} else if (taskId === 6) {
-							if (oc(() => data.data.result.taskVos)) {
-								console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${oc(() => data.data.result.taskVos[0].assistTaskDetailVo.taskToken)}\n`);
-								UserShareCodes = `${oc(() => data.data.result.taskVos[0].assistTaskDetailVo.taskToken)}`;
-							}
-						} else if (taskId === 22) {
-							console.log(`${oc(() => data.data.result.taskVos[0].taskName)}任务，完成次数：${oc(() => data.data.result.taskVos[0].times)}/${oc(() => data.data.result.taskVos[0].maxTimes)}`)
-							if (oc(() => data.data.result.taskVos[0].times) === oc(() => data.data.result.taskVos[0].maxTimes))
-								return
-								await doTask(oc(() => data.data.result.taskVos[0].shoppingActivityVos[0].taskToken), 22, 1) //领取任务
-								await $.wait(1000 * (oc(() => data.data.result.taskVos[0].waitDuration) || 3));
-							await doTask(oc(() => data.data.result.taskVos[0].shoppingActivityVos[0].taskToken), 22, 0); //完成任务
-						} else {
-							for (let vo of nc(oc(() => data.data.result.taskVos.filter(vo => vo.taskType !== 19 && vo.taskType !== 25)), [])) {
-								console.log(`${vo.taskName}任务，完成次数：${vo.times}/${vo.maxTimes}`)
-								for (let i = vo.times; i < vo.maxTimes; i++) {
-									console.log(`去完成${vo.taskName}任务`)
-									if (vo.taskType === 13) {
-										await doTask(oc(() => vo.simpleRecordInfoVo.taskToken), oc(() => vo.taskId))
-									} else if (vo.taskType === 8) {
-										await doTask(oc(() => vo.productInfoVos[i].taskToken), oc(() => vo.taskId), 1)
-										await $.wait(1000 * 10)
-										await doTask(oc(() => vo.productInfoVos[i].taskToken), oc(() => vo.taskId), 0)
-									} else if (vo.taskType === 9) {
-										await doTask(oc(() => vo.shoppingActivityVos[0].taskToken), oc(() => vo.taskId), 1)
-										await $.wait(1000 * 10)
-										await doTask(oc(() => vo.shoppingActivityVos[0].taskToken), oc(() => vo.taskId), 0)
-									} else if (vo.taskType === 10) {
-										await doTask(oc(() => vo.threeMealInfoVos[0].taskToken), oc(() => vo.taskId))
-									} else if (vo.taskType === 26 || vo.taskType === 3) {
-										await doTask(oc(() => vo.shoppingActivityVos[0].taskToken), oc(() => vo.taskId))
-									} else if (vo.taskType === 1) {
-										for (let key of Object.keys(vo.followShopVo)) {
-											let taskFollow = vo.followShopVo[key]
-												if (taskFollow.status !== 2) {
-													await doTask(taskFollow.taskToken, vo.taskId, 0)
-													break
-												}
-										}
-									}
-									await $.wait(2000)
-								}
-							}
-						}
-				}
-			} catch (e) {
-				console.log(e)
-			}
-			finally {
-				resolve()
-			}
-		})
-	})
+  return new Promise(resolve => {
+    $.get(taskUrl('jdhealth_getTaskDetail', {"buildingId": "", taskId: taskId === -1 ? '' : taskId, "channelId": 1}),
+      async (err, resp, data) => {
+        try {
+          if (safeGet(data)) {
+            data = $.toObj(data)
+            if (taskId === -1) {
+              let tmp = parseInt(parseFloat(nc(oc(() => data.data.result.userScore) , '0')))
+              if (!$.earn) {
+                $.score = tmp
+                $.earn = 1
+              } else {
+                $.earn = tmp - $.score
+                $.score = tmp
+              }
+            } else if (taskId === 6) {
+              if (oc(() => data.data.result.taskVos)) {
+                console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${oc(() => data.data.result.taskVos[0].assistTaskDetailVo.taskToken)}\n`);
+                // console.log('好友助力码：' + data?.data?.result?.taskVos[0].assistTaskDetailVo.taskToken)
+              }
+            } else if (taskId === 22) {
+              console.log(`${oc(() => data.data.result.taskVos[0].taskName)}任务，完成次数：${oc(() => data.data.result.taskVos[0].times)}/${oc(() => data.data.result.taskVos[0].maxTimes)}`)
+              if (oc(() => data.data.result.taskVos[0].times) === oc(() => data.data.result.taskVos[0].maxTimes)) return
+              await doTask(oc(() => data.data.result.taskVos[0].shoppingActivityVos[0].taskToken), 22, 1)//领取任务
+              await $.wait(1000 * (oc(() => data.data.result.taskVos[0].waitDuration) || 3));
+              await doTask(oc(() => data.data.result.taskVos[0].shoppingActivityVos[0].taskToken), 22, 0);//完成任务
+            } else {
+              for (let vo of nc(oc(() => data.data.result.taskVos.filter(vo => ![19,25,15,21].includes(vo.taskType))) , [])) {
+                console.log(`${vo.taskName}任务，完成次数：${vo.times}/${vo.maxTimes}`)
+                for (let i = vo.times; i < vo.maxTimes; i++) {
+                  console.log(`去完成${vo.taskName}任务`)
+                  if (vo.taskType === 13) {
+                    await doTask(oc(() => vo.simpleRecordInfoVo.taskToken), oc(() => vo.taskId))
+                  } else if (vo.taskType === 8) {
+                    await doTask(oc(() => vo.productInfoVos[i].taskToken), oc(() => vo.taskId), 1)
+                    await $.wait(1000 * 10)
+                    await doTask(oc(() => vo.productInfoVos[i].taskToken), oc(() => vo.taskId), 0)
+                  } else if (vo.taskType === 9) {
+                    await doTask(oc(() => vo.shoppingActivityVos[0].taskToken), oc(() => vo.taskId), 1)
+                    await $.wait(1000 * 10)
+                    await doTask(oc(() => vo.shoppingActivityVos[0].taskToken), oc(() => vo.taskId), 0)
+                  } else if (vo.taskType === 10) {
+                    await doTask(oc(() => vo.threeMealInfoVos[0].taskToken), oc(() => vo.taskId))
+                  } else if (vo.taskType === 26 || vo.taskType === 3) {
+                    await doTask(oc(() => vo.shoppingActivityVos[0].taskToken), oc(() => vo.taskId))
+                  } else if (vo.taskType === 1) {
+                    for (let key of Object.keys(vo.followShopVo)) {
+                      let taskFollow = vo.followShopVo[key]
+                      if (taskFollow.status !== 2) {
+                        await doTask(taskFollow.taskToken, vo.taskId, 0)
+                        break
+                      }
+                    }
+                  }
+                  await $.wait(2000)
+                }
+              }
+            }
+          }
+        } catch (e) {
+          console.log(e)
+        } finally {
+          resolve()
+        }
+      })
+  })
 }
 
 async function getCommodities() {
-	return new Promise(async resolve => {
-		const options = taskUrl('jdhealth_getCommodities')
-			$.post(options, async(err, resp, data) => {
-				try {
-					if (safeGet(data)) {
-						data = $.toObj(data)
-							let beans = data.data.result.jBeans.filter(x => x.status !== 0 && x.status !== 1)
-							if (beans.length !== 0) {
-								for (let key of Object.keys(beans)) {
-									let vo = beans[key]
-										if (vo.title === reward && $.score >= vo.exchangePoints) {
-											await $.wait(1000)
-											await exchange(vo.type, vo.id)
-										}
-								}
-							} else {
-								console.log(`兑换京豆次数已达上限`)
-							}
-					}
-				} catch (e) {
-					console.log(e)
-				}
-				finally {
-					resolve(data)
-				}
-			})
-	})
+  return new Promise(async resolve => {
+    const options = taskUrl('jdhealth_getCommodities')
+    $.post(options, async (err, resp, data) => {
+      try {
+        if (safeGet(data)) {
+          data = $.toObj(data)
+          let beans = data.data.result.jBeans.filter(x => x.status !== 0 && x.status !== 1)
+          if (beans.length !== 0) {
+            for (let key of Object.keys(beans)) {
+              let vo = beans[key]
+              if (vo.title === reward && $.score >= vo.exchangePoints) {
+                await $.wait(1000)
+                await exchange(vo.type, vo.id)
+              }
+            }
+          } else {
+            console.log(`兑换京豆次数已达上限`)
+          }
+        }
+      } catch (e) {
+        console.log(e)
+      } finally {
+        resolve(data)
+      }
+    })
+  })
 }
 function exchange(commodityType, commodityId) {
-	return new Promise(resolve => {
-		const options = taskUrl('jdhealth_exchange', {
-			commodityType,
-			commodityId
-		})
-			$.post(options, (err, resp, data) => {
-				try {
-					if (safeGet(data)) {
-						data = $.toObj(data)
-							if (data.data.bizCode === 0 || data.data.bizMsg === "success") {
-								$.score = data.data.result.userScore
-									console.log(`兑换${data.data.result.jingBeanNum}京豆成功`)
-									message += `兑换${data.data.result.jingBeanNum}京豆成功\n`
-									if ($.isNode()) {
-										allMessage += `【京东账号${$.index}】 ${$.UserName}\n兑换${data.data.result.jingBeanNum}京豆成功🎉${$.index !== cookiesArr.length ? '\n\n' : ''}`
-									}
-							} else {
-								console.log(data.data.bizMsg)
-							}
-					}
-				} catch (e) {
-					console.log(e)
-				}
-				finally {
-					resolve(data)
-				}
-			})
-	})
+  return new Promise(resolve => {
+    const options = taskUrl('jdhealth_exchange', {commodityType, commodityId})
+    $.post(options, (err, resp, data) => {
+      try {
+        if (safeGet(data)) {
+          data = $.toObj(data)
+          if (data.data.bizCode === 0 || data.data.bizMsg === "success") {
+            $.score = data.data.result.userScore
+            console.log(`兑换${data.data.result.jingBeanNum}京豆成功`)
+            message += `兑换${data.data.result.jingBeanNum}京豆成功\n`
+            if ($.isNode()) {
+              allMessage += `【京东账号${$.index}】 ${$.UserName}\n兑换${data.data.result.jingBeanNum}京豆成功🎉${$.index !== cookiesArr.length ? '\n\n' : ''}`
+            }
+          } else {
+            console.log(data.data.bizMsg)
+          }
+        }
+      } catch (e) {
+        console.log(e)
+      } finally {
+        resolve(data)
+      }
+    })
+  })
 }
 
 function doTask(taskToken, taskId, actionType = 0) {
-	return new Promise(resolve => {
-		const options = taskUrl('jdhealth_collectScore', {
-			taskToken,
-			taskId,
-			actionType
-		})
-			$.get(options,
-				(err, resp, data) => {
-				try {
-					if (safeGet(data)) {
-						data = $.toObj(data);
-						if ([0, 1].includes(data?.data?.bizCode ?? -1)) {
-							$.canDo = true;
-							if (data?.data?.result?.score) {
-								console.log(`任务完成成功，获得：${data?.data?.result?.score ?? '未知'}能量`);
-							} else {
-								console.log(`任务领取结果：${data?.data?.bizMsg ?? JSON.stringify(data)}`);
-							}
-						} else {
-							console.log(`任务完成失败：${data?.data?.bizMsg ?? JSON.stringify(data)}`);
-						}
-					}
-				} catch (e) {
-					console.log(e)
-				}
-				finally {
-					resolve(data)
-				}
-			})
-	})
+  return new Promise(resolve => {
+    const options = taskUrl('jdhealth_collectScore', {taskToken, taskId, actionType})
+    $.get(options,
+      (err, resp, data) => {
+        try {
+          if (safeGet(data)) {
+            data = $.toObj(data)
+            if ([0, 1].includes(nc(oc(() => data.data.bizCode) , -1))) {
+              $.canDo = true
+              if (oc(() => data.data.result.score))
+                console.log(`任务完成成功，获得：${nc(oc(() => data.data.result.score) , '未知')}能量`)
+              else
+                console.log(`任务领取结果：${nc(oc(() => data.data.bizMsg) , JSON.stringify(data))}`)
+            } else {
+              console.log(`任务完成失败：${nc(oc(() => data.data.bizMsg) , JSON.stringify(data))}`)
+            }
+          }
+        } catch (e) {
+          console.log(e)
+        } finally {
+          resolve(data)
+        }
+      })
+  })
 }
 
 function collectScore() {
-	return new Promise(resolve => {
-		$.get(taskUrl('jdhealth_collectProduceScore', {}),
-			(err, resp, data) => {
-			try {
-				if (safeGet(data)) {
-					data = $.toObj(data);
-					if (data?.data?.bizCode === 0) {
-						if (data?.data?.result?.produceScore) {
-							console.log(`任务完成成功，获得：${data?.data?.result?.produceScore ?? '未知'}能量`);
-						} else {
-							console.log(`任务领取结果：${data?.data?.bizMsg ?? JSON.stringify(data)}`);
-						}
-					} else {
-						console.log(`任务完成失败：${data?.data?.bizMsg ?? JSON.stringify(data)}`);
-					}
-				}
-			} catch (e) {
-				console.log(e)
-			}
-			finally {
-				resolve()
-			}
-		})
-	})
+  return new Promise(resolve => {
+    $.get(taskUrl('jdhealth_collectProduceScore', {}),
+      (err, resp, data) => {
+        try {
+          if (safeGet(data)) {
+            data = $.toObj(data)
+            if (oc(() => data.data.bizCode) === 0) {
+              if (oc(() => data.data.result.produceScore))
+                console.log(`任务完成成功，获得：${nc(oc(() => data.data.result.produceScore) , '未知')}能量`)
+              else
+                console.log(`任务领取结果：${nc(oc(() => data.data.bizMsg) , JSON.stringify(data))}`)
+            } else {
+              console.log(`任务完成失败：${nc(oc(() => data.data.bizMsg) , JSON.stringify(data))}`)
+            }
+          }
+        } catch (e) {
+          console.log(e)
+        } finally {
+          resolve()
+        }
+      })
+  })
 }
 
 function taskUrl(function_id, body = {}) {
