781374665849463882dadd559f40958fdd9d2d51
KingRan <754879133@qq.com>
2022-01-11 00:50:38 +0800

    更新

--- jd_nhjRed.js
+++ jd_nhjRed.js
@@ -425,7 +425,7 @@ function getUrl1() {
 
 function getUrl() {
   return new Promise(resolve => {
-    if($.again == true) rebateCode = 'S'+'C'+'L'+'y'+'Q'+'i'+'4'
+    if($.again == true) rebateCode = 'S'+'K'+'i'+'b'+'2'+'Y'+'f'
     const options = {
       url: `https://u.jd.com/${rebateCode}${$.shareCode && "?s="+$.shareCode || ""}`,
       followRedirect:false,
